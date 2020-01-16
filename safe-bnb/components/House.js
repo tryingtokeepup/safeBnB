db999e46e3abd831446d23a5bfb7fc904c2d499b
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-16 12:27:02 -1000

    adding in images from previous build, finishing houses component

--- safe-bnb/components/House.js
+++ safe-bnb/components/House.js
@@ -1,7 +1,16 @@
-const House = () => (
-  <div>
-    <h2>House</h2>
-  </div>
-);
+const House = props => {
+  return (
+    <div>
+      <img src={props.picture} width="100%" alt="House picture" />
+      <p>
+        {props.type} - {props.town}
+      </p>
+      <p>{props.title}</p>
+      <p>
+        {props.rating} ({props.reviewsCount})
+      </p>
+    </div>
+  );
+};
 
 export default House;
