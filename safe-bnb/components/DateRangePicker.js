fe5dbe798d33f7be9046f417eab319f6545ccb27
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-27 22:03:32 -1000

    adding in styling for date component, checking production

--- safe-bnb/components/DateRangePicker.js
+++ safe-bnb/components/DateRangePicker.js
@@ -1,7 +1,33 @@
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 
 export default () => (
-  <div>
-    <DayPickerInput />
+  <div className="date-range-picker-container">
+    <div>
+      <label>From:</label>
+      <DayPickerInput />
+    </div>
+    <div>
+      <label>To:</label>
+      <DayPickerInput />
+    </div>
+    <style jsx>{`
+      .date-range-picker-container div {
+        display: grid;
+        border: 1px solid #ddd;
+        grid-template-columns: 30% 70%;
+        padding: 10px;
+      }
+
+      label {
+        padding-top: 10px;
+      }
+    `}</style>
+    <style jsx global>{`
+      .DayPickerInput input {
+        width: 120px;
+        padding: 10px;
+        font-size: 1rem;
+      }
+    `}</style>
   </div>
 );
