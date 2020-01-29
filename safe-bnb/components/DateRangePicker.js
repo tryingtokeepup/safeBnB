75dd1e6b8dffed953e916218023a2b8ed5788533
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-28 23:09:59 -1000

    adding in and syncing dates for the datepicker component

--- safe-bnb/components/DateRangePicker.js
+++ safe-bnb/components/DateRangePicker.js
@@ -1,14 +1,52 @@
 import DayPickerInput from 'react-day-picker/DayPickerInput';
+import dateFNSFormat from 'date-fns/format';
+import dateFNSParse from 'date-fns/parse';
+import { DateUtils } from 'react-day-picker';
+
+const parseDate = (string, format, locale) => {
+  const parsed = dateFNSParse(string, format, newDate(), { locale });
+  // check if parsed is date, and if so, pass parsed in, and if not, null it
+  return DateUtils.isDate(parsed) ? parsed : null;
+};
+
+const formatDate = (date, format, locale) => {
+  dateFNSFormat(date, format, { locale });
+};
+const format = 'dd MMM yyyy';
 
 export default () => (
   <div className="date-range-picker-container">
     <div>
       <label>From:</label>
-      <DayPickerInput />
+      <DayPickerInput
+        formatDate={formatDate}
+        format={format}
+        parseDate={parseDate}
+        placeholder={`${dateFNSFormat(new Date(), format)}`}
+        dayPickerProps={{
+          modifiers: {
+            disabled: {
+              before: new Date()
+            }
+          }
+        }}
+      />
     </div>
     <div>
       <label>To:</label>
-      <DayPickerInput />
+      <DayPickerInput
+        formatDate={formatDate}
+        format={format}
+        parseDate={parseDate}
+        placeholder={`${dateFNSFormat(new Date(), format)}`}
+        dayPickerProps={{
+          modifiers: {
+            disabled: {
+              before: new Date()
+            }
+          }
+        }}
+      />
     </div>
     <style jsx>{`
       .date-range-picker-container div {
