bfa645fd199b5358925984838821df5e059aab6a
tryingtokeepup <kl.hawaii@gmail.com>
2020-02-05 22:13:25 -1000

    fleshing out login modal

--- safe-bnb/components/LoginModal.js
+++ safe-bnb/components/LoginModal.js
@@ -1,1 +1,14 @@
-export default () => <p>Login Modal</p>;
+export default () => (
+  // add in a fragment here
+  <>
+    <h2>Log in here!</h2>
+    <div>
+      <form>
+        <input id="email" type="email" placeholder="Email Address" />
+
+        <input id="password" type="password" placeholder="Password" />
+        <button>Log in. :)</button>
+      </form>
+    </div>
+  </>
+);
