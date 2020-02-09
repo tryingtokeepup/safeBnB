79ac0a467745341a26875d1d28b0ef89b1250f07
tryingtokeepup <kl.hawaii@gmail.com>
2020-02-08 23:33:10 -1000

    need to learn more about styling; so far so good on registration modal

--- safe-bnb/components/RegistrationModal.js
+++ safe-bnb/components/RegistrationModal.js
@@ -1,1 +1,17 @@
-export default () => <p>Registration Modal</p>;
+export default () => (
+  <>
+    <h2>Sign Up</h2>
+    <div>
+      <form>
+        <input id="email" type="email" placeholder="Email address" />
+        <input id="password" type="password" placeholder="Password" />
+        <input
+          id="passwordconfirmation"
+          type="password"
+          placeholder="Please confirm password!"
+        />
+        <button>Sign up! :D</button>
+      </form>
+    </div>
+  </>
+);
