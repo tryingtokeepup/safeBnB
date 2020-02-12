78e6043afcfce15d8c58ce2eefdcdb3004999886
tryingtokeepup <kl.hawaii@gmail.com>
2020-02-11 23:49:27 -1000

    adding store to _app.js, so store is accessable throughout app

--- safe-bnb/pages/_app.js
+++ safe-bnb/pages/_app.js
@@ -1,6 +1,12 @@
 import 'react-day-picker/lib/style.css';
+import { StoreProvider } from 'easy-peasy';
 
+import store from '../store';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StoreProvider store={store}>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
