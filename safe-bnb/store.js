5b6444cba4998a24147eb6d1e1c16bb0b20c3490
tryingtokeepup <kl.hawaii@gmail.com>
2020-02-12 23:32:20 -1000

    adding in modal states into state.js, will refactor code in header and other modals

--- safe-bnb/store.js
+++ safe-bnb/store.js
@@ -1,2 +1,26 @@
 import { createStore, action } from 'easy-peasy';
-export default createStore({});
+import { formatRelativeWithOptions } from 'date-fns/fp';
+export default createStore({
+  modals: {
+    // set all modal states as false, make functions to trigger states to true
+    showModal: false,
+    showLoginModal: false,
+    showRegistrationModal: false,
+    setShowModal: action(state => {
+      state.showModal = true;
+    }),
+    setHideModal: action(state => {
+      state.showModal = false;
+    }),
+    setShowLoginModal: action(state => {
+      state.showModal = true;
+      state.showLoginModal = true;
+      state.showRegistrationModal = false;
+    }),
+    setShowRegistrationModal: action(state => {
+      state.showModal = true;
+      state.showLoginModal = false;
+      state.showRegistrationModal = true;
+    })
+  }
+});
